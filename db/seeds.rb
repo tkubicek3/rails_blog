aafd0325a58d53f48659cb0c55785939790bde4a
Tomas Kubicek <tkubicek3@gmail.com>
2015-12-07 22:04:23 +0100

    Before optim.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,5 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 [:admin, :user].each do |role|
-<<<<<<< HEAD
   Role.find_or_create_by(name: role, without_protection: true)
-=======
-  Role.find_or_create_by_name({ name: role }, without_protection: true)
->>>>>>> 400f1a416d286e3dc2720400d5049cbf0d7b34ac
 end
